7ae2b69813b4ab3ea7f0d88752979d61a79a0e0d
victor.lisboa.ps <victor.lisboa.ps@gjc.com.br>
2023-03-16 10:14:44 -0300

    aff

--- custom/init.lua
+++
@@ -1,4 +0,0 @@
-vim.opt.relativenumber = true
-vim.opt.guicursor = "i:block"
--- vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format(nil, 5000)]]
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
